74f97e2ae545b3be70bf8e3973e60a548e86f432
ubaimalang <35678928+ubaimalang@users.noreply.github.com>
2020-01-30 14:27:27 +0700

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,3 +1,4 @@
+// with for
 function MinMax(arr) {
    let minIndex = 0;
    let maxIndex = 1;
@@ -14,6 +15,29 @@ function MinMax(arr) {
        maxIndex = i;
      }
    }
+   return [minIndex,maxIndex]
+ }
+
+ console.log(MinMax([9, 4, -1, 2, 7, 8, 0, 11]))
+// with do..while
+function MinMax(arr) {
+   let minIndex = 0;
+   let maxIndex = 1;
+   let min = arr[0];
+   let max = arr[1];
+
+   let i = 0;
+do {
+ if (arr[i] < min) {
+       min = arr[i];
+       minIndex = i;
+     }
+     if (arr[i] > max) {
+       max = arr[i]
+       maxIndex = i;
+     }
+i++;
+} while(i < arr.length);
    return [minIndex,maxIndex]
  }
 
